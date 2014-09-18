8c1f93f564283c2bc1a5ae54bc0b78912a871555
marcelinol <marcelinolucianom@gmail.com>
2014-09-18 16:01:54 -0300

    js refactoring

--- rd-js-integration.js
+++ rd-js-integration.js
@@ -1,106 +1,117 @@
-//GOOGLE ANALYTICS
-function read_cookie(a) {
+function RdJsIntegration(token_rdstation, identificador){
+  var self = this;
+  this.token_rdstation = token_rdstation;
+  this.identificador = identificador;
+  this.doIt = function(){
+    setJQuery();
+    waitForLoadJQuery(prepareData);
+  }
+
+  //GOOGLE ANALYTICS
+  function read_cookie(a) {
     var b = a + '=';
     var c = document.cookie.split(';');
     for (var d = 0; d < c.length; d++) {
-        var e = c[d];
-        while (e.charAt(0) == ' ') e = e.substring(1, e.length);
-        if (e.indexOf(b) == 0) {
-            return e.substring(b.length, e.length)
-        }
+      var e = c[d];
+      while (e.charAt(0) == ' ') e = e.substring(1, e.length);
+      if (e.indexOf(b) == 0) {
+        return e.substring(b.length, e.length)
+      }
     }
     return null
-};
-//
+  };
+  //
 
-function setJQuery(token_rdstation, identificador){
-  if (typeof jQuery == "undefined"){
-    var theNewScript = document.createElement("script");
-    theNewScript.type = "text/javascript";
-    theNewScript.src = "http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js";
-    document.getElementsByTagName("head")[0].appendChild(theNewScript);
+  function setJQuery() {
+    if (typeof jQuery == "undefined"){
+      var theNewScript = document.createElement("script");
+      theNewScript.type = "text/javascript";
+      theNewScript.src = "http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js";
+      document.getElementsByTagName("head")[0].appendChild(theNewScript);
+    }
+    return false;
   }
-  return false;
-}
 
-  function waitForLoad() {
+  function waitForLoadJQuery(callback) {
     if (typeof jQuery != "undefined") {
-      return true;
-    } else {
-        window.setTimeout(waitForLoad, 1000);
+      callback();
+    } else{ 
+      window.setTimeout(function () { waitForLoadJQuery(callback) }, 1000);
     }
   }
 
-function getSubmit(token_rdstation, identificador) {
+  function prepareData() {
     var inputEmail = ['email', 'e-mail', 'e_mail', 'email_lead'],
+        form_data_original = [],
         form_data = [],
         form;
     $(document).ready(function() {
       $(':submit').click(function(event) {
         form = $(this).closest('form');
         if (!form) {
-            return;
+          return;
         }
         function isHidden(element) { return $(element).is("input[type=hidden]") };
         function isPassword(element) { return $(element).is("input[type=password]") };
         form = form.children().map(function() {
           if (!(isHidden(this) || isPassword(this))) { return this; }
         });
-        form_data = form.serializeArray();
+        form_data_original = form.serializeArray();
+        
+        var inputPassword = ['captcha','_wpcf7', '_wpcf7_version', '_wpcf7_unit_tag', '_wpnonce', '_wpcf7_is_ajax_call', '_wpcf7_locale'];
+        for (var i in form_data_original) {
+          if (form_data_original.hasOwnProperty(i)) {
+            if(inputPassword.indexOf(form_data_original[i].name.toLowerCase()) == -1){
+              form_data.push(form_data_original[i]);
+            }
+          }
+        }
+        var identificador_obj = {
+          'name': 'identificador',
+          'value': self.identificador
+        },
+        token_obj = {
+          'name': 'token_rdstation',
+          'value': self.token_rdstation
+        },
+        c_utmz_obj = {
+          'name': 'c_utmz',
+          'value': read_cookie('__utmz')
+        };
+        form_data.push(identificador_obj, token_obj, c_utmz_obj);
+        
         for (var i in form_data) {
           if (form_data.hasOwnProperty(i)) {
             if(inputEmail.indexOf(form_data[i].name.toLowerCase()) != -1){
               form_data[i].name = 'email';
-              postData(form, form_data, token_rdstation, identificador);
+              postData(form, form_data);
+              event.preventDefault();
+              return;
             }
           }
         }
-        event.preventDefault();
       });
-    });
+});
 }
 
-function postData(form, form_data_original, token_rdstation, identificador) {
-    var inputPassword = ['captcha','_wpcf7', '_wpcf7_version', '_wpcf7_unit_tag', '_wpnonce', '_wpcf7_is_ajax_call', '_wpcf7_locale'];
-        form_data = [];
-    for (var i in form_data_original) {
-      if (form_data_original.hasOwnProperty(i)) {
-        if(inputPassword.indexOf(form_data_original[i].name.toLowerCase()) == -1){
-          form_data.push(form_data_original[i]);
-        }
-      }
+function postData(form, form_data) {
+  $.ajax({
+    type: 'POST',
+    url: 'https://www.rdstation.com.br/api/1.2/conversions',
+    data: form_data,
+    crossDomain: true,
+    error: function(response) {
+      console.log(response);
+    },
+    complete: function() {
+      form.submit();
     }
-    var identificador_obj = {
-            'name': 'identificador',
-            'value': identificador
-        },
-        token_obj = {
-            'name': 'token_rdstation',
-            'value': token_rdstation
-        },
-        c_utmz_obj = {
-            'name': 'c_utmz',
-            'value': read_cookie('__utmz')
-        };
-    form_data.push(identificador_obj, token_obj, c_utmz_obj);
-    $.ajax({
-        type: 'POST',
-        url: 'https://www.rdstation.com.br/api/1.2/conversions',
-        data: form_data,
-        crossDomain: true,
-        error: function(response) {
-            console.log(response);
-        },
-        complete: function() {
-            form.submit();
-        }
-    });
+  });
+}
 }
 
 function RDStationFormIntegration(token_rdstation, identificador) {
-  setJQuery();
-  if(waitForLoad()){
-    getSubmit(token_rdstation, identificador);
-  }
-
+  var integration = new RdJsIntegration(token_rdstation, identificador);
+  integration.doIt();
 }
+
